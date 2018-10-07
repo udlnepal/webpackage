c8f25fd818c8fad7b84515ff7df2760591d78558
Sumit <timusumit@gmail.com>
2018-10-07 17:05:10 +0545

    okaye
    package updated

--- application/logs/log-2018-10-07.php
+++ application/logs/log-2018-10-07.php
@@ -13187,3 +13187,23 @@ DEBUG - 2018-10-07 13:18:17 --> File loaded: E:\xampp\htdocs\webpackage\applicat
 DEBUG - 2018-10-07 13:18:17 --> File loaded: E:\xampp\htdocs\webpackage\application\modules/admin/views/templates/footer.php
 INFO - 2018-10-07 13:18:17 --> Final output sent to browser
 DEBUG - 2018-10-07 13:18:17 --> Total execution time: 1.0581
+INFO - 2018-10-07 13:19:35 --> Config Class Initialized
+INFO - 2018-10-07 13:19:35 --> Hooks Class Initialized
+DEBUG - 2018-10-07 13:19:35 --> UTF-8 Support Enabled
+INFO - 2018-10-07 13:19:35 --> Utf8 Class Initialized
+INFO - 2018-10-07 13:19:35 --> URI Class Initialized
+DEBUG - 2018-10-07 13:19:35 --> No URI present. Default controller set.
+INFO - 2018-10-07 13:19:35 --> Router Class Initialized
+INFO - 2018-10-07 13:19:35 --> Output Class Initialized
+INFO - 2018-10-07 13:19:35 --> Security Class Initialized
+DEBUG - 2018-10-07 13:19:35 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-10-07 13:19:35 --> Input Class Initialized
+INFO - 2018-10-07 13:19:35 --> Language Class Initialized
+INFO - 2018-10-07 13:19:36 --> Language Class Initialized
+INFO - 2018-10-07 13:19:36 --> Config Class Initialized
+INFO - 2018-10-07 13:19:36 --> Loader Class Initialized
+INFO - 2018-10-07 13:19:36 --> Controller Class Initialized
+DEBUG - 2018-10-07 13:19:36 --> Test MX_Controller Initialized
+DEBUG - 2018-10-07 13:19:36 --> File loaded: E:\xampp\htdocs\webpackage\application\modules/Test/views/test.php
+INFO - 2018-10-07 13:19:36 --> Final output sent to browser
+DEBUG - 2018-10-07 13:19:36 --> Total execution time: 0.7580
